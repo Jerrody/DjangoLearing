20a205a1add5d98d645b6274e94e8571e45e1dbb
Jerrody <jerrody.hipkins@gmail.com>
2020-07-31 05:26:08 +0300

    Some Changes

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,7 @@ from blog.views import HomeView
 from . import views
 
 urlpatterns = [
-    #Category
     path('', views.HomeView.as_view()),
     path('<slug:slug>/', views.CategoryView.as_view(), name='category'),
-    #Post
     path('<slug:slug>/', views.PostView.as_view(), name='post'),
 ]
